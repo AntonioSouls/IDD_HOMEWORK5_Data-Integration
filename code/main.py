6406763344e42c7357529e82c41023065ebb5b50
danielluca28 <dani28settembre@gmail.com>
2025-01-27 14:15:52 +0100

    provided descriptions for each attribute of each source file

--- code/main.py
+++ code/main.py
@@ -1,10 +1,13 @@
-import mediated_schema_creation
 import time
+import os
+
+# Definisci il percorso della cartella 'sources' rispetto alla cartella 'code'
+sources_path = os.path.join(os.path.dirname(__file__), '..', 'sources')
 
 # Funzione principale che orchestra l'intera esecuzione del programma
 def main():
     start_time = time.time()
-    mediated_schema_creation.schema_creator()
+    # call function for mediated schema creation
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Mediated-Schema created in {execution_time:.6f} secondi")
